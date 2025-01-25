ad9b2dfb551c19c46a7a0106a9310e93e39d8f5b
Christoffer Fridlund <christoffer.fridlund@gmail.com>
2025-01-25 20:56:25 +0200

    Disable bubble placeholder graphics.

--- godot/scenes/disable_debug_visuals.gd
+++ godot/scenes/disable_debug_visuals.gd
@@ -1,1 +1,8 @@
 extends Node2D
+
+@export var spriteHolders: Array[Node2D] = []
+
+func _ready() -> void:
+	for holder in spriteHolders:
+		for child in holder.get_children():
+			child.texture = null
