d48196bb4fced421fdbb15c12ab036fd8beaacdd
Anders Forsman <andeye@cyancore.net>
2025-01-26 01:20:20 +0200

    Add balloon scene copy for dialogue with balloon font set

--- godot/scenes/animations/animation_intro_scene.gd
+++ godot/scenes/animations/animation_intro_scene.gd
@@ -3,12 +3,16 @@ extends Node2D
 @export var dialogue_resource: DialogueResource
 @export var dialgoue_start: String = "start"
 
+const Balloon = preload("res://dialogue/balloon.tscn")
+
 func start() -> void:
 	print("Starting animation!")
 	#DialogueManager.show_example_dialogue_balloon(dialogue_resource, dialgoue_start)
 	var cb1 = func():
 		var cb2 = func():
-			DialogueManager.show_example_dialogue_balloon(dialogue_resource, dialgoue_start)
+			var balloon: Node = Balloon.instantiate()
+			get_tree().current_scene.add_child(balloon)
+			balloon.start(dialogue_resource, dialgoue_start)
 		$"../Employee".move_to_position($"../FixedAnchors/EmployeeDiscussionPoint".position, cb2)
 	$"../Manager".move_to_position($"../FixedAnchors/ManagerDiscussionPoint".position, cb1)
 	
