7bd098549a52b9926fe18381c8cb97a03561e2c3
Christoffer Fridlund <christoffer.fridlund@gmail.com>
2025-01-25 17:44:12 +0200

    Cleaning up in the file names.

--- godot/scenes/animations/animation_intro_scene.gd
+++ godot/scenes/animations/animation_intro_scene.gd
@@ -9,11 +9,11 @@ func start() -> void:
 	var cb1 = func():
 		var cb2 = func():
 			DialogueManager.show_example_dialogue_balloon(dialogue_resource, dialgoue_start)
-		$"../Worker/Node2D2".move_to_position($"../MoveAnchors/EmployeeDiscussionPoint".position, cb2)
-	$"../Manager/Node2D2".move_to_position($"../MoveAnchors/ManagerDiscussionPoint".position, cb1)
+		$"../Employee/Node2D2".move_to_position($"../FixedAnchors/EmployeeDiscussionPoint".position, cb2)
+	$"../Manager/Node2D2".move_to_position($"../FixedAnchors/ManagerDiscussionPoint".position, cb1)
 	
 func manager_exit() -> void:
 	var cb1 = func():
 		pass
-	$"../Manager/Node2D2".move_to_position(Vector2(-100, 0), cb1)
+	$"../Manager/Node2D2".move_to_position($"../FixedAnchors/ManagerSpawnPoint".position, cb1)
 	
