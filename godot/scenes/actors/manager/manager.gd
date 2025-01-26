889474210a9a535cd25139b1bbc0cc379101e635
Anders Forsman <andeye@cyancore.net>
2025-01-26 16:52:36 +0200

    Get manager to shut up when he leaves

--- godot/scenes/actors/manager/manager.gd
+++ godot/scenes/actors/manager/manager.gd
@@ -1,7 +1,10 @@
 extends Movable
 
+@export var audio_player: AudioStreamPlayer
+
 func manager_exit() -> void:
 	var cb1 = func():
 		print("Manager has exited the building")
 		$"../CustomerSpawner".spawn_new_customer()
+		audio_player.stop()
 	self.move_to_position($"../FixedAnchors/ManagerSpawnPoint".position, cb1)
